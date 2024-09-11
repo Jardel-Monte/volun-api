488666d73c050ac34ea7c24960a4cb613cfad796
Anderson Brito Santos <141651296+andersonbs96@users.noreply.github.com>
2024-09-11 10:50:41 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 
 // Middleware para parsing do body em JSON
