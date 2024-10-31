60f3417b783d4a529cb144448ca2aa146908d9c2
KevinJota <141651714+KevinJota@users.noreply.github.com>
2024-10-31 11:48:24 -0300

    Update nova Rota p/ organizacao
    endpoint para utilizar novo método:  URL+organizacao/criador/:criador_id
    este método irá retornar todos as organizações relacionadas a um usuario que criou elas

--- routes/organizacaoRoutes.js
+++ routes/organizacaoRoutes.js
@@ -7,6 +7,8 @@ router.get('/', organizacaoController.getOrganizacoes);
 router.get('/:id', organizacaoController.getOrganizacaoById);
 router.put('/:id', organizacaoController.updateOrganizacao);
 router.delete('/:id', organizacaoController.deleteOrganizacao);
+router.get('/criador/:criador_id', organizacaoController.getOrganizacaoByCriadorId);
+
 
 module.exports = router;
 
