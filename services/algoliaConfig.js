0468b68784241cece024f04daa57a8706c1e5fbf
Jardel-Monte <JardelLima23@gmail.com>
2024-10-31 10:25:23 -0300

    Update algoliaConfig.js

--- services/algoliaConfig.js
+++ services/algoliaConfig.js
@@ -1,7 +1,7 @@
 const algoliasearch = require('algoliasearch');
 
 const ALGOLIA_APP_ID = 'JUIX37JGYV';
-const ALGOLIA_API_KEY = '367b1d2fcbb08cb101c34019bf40e57b';
+const ALGOLIA_API_KEY = '5c3c8bdb4ff9e4fc1367d6047a3e930f';
 const ALGOLIA_INDEX_NAME = 'eventos';
 
 const searchClient = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
