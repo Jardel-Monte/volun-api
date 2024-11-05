bc1b2c0016dccf7e12435662cd125e5dd82e6443
Jardel-Monte <JardelLima23@gmail.com>
2024-11-05 10:47:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');  
 const app = require('./App');
+const sincronizarEventosComAlgolia = require('./services/syncAlgolia');
 
 mongoose.set('strictQuery', false);
 
@@ -10,8 +11,17 @@ const PORT = process.env.PORT || 3000;
 const mongoUri = 'mongodb+srv://volunzinho:S8EXiRDrAwejYDO0@volun-api.dqoev.mongodb.net/'
 
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
+  .then(async () => {
     console.log('Conectado ao MongoDB com sucesso');
+
+    // Sincronizar dados com Algolia após conectar ao MongoDB
+    try {
+      await sincronizarEventosComAlgolia();
+      console.log('Sincronização com Algolia concluída com sucesso.');
+    } catch (error) {
+      console.error('Erro ao sincronizar com Algolia:', error);
+    }
+
     app.listen(PORT, () => {
       console.log(`Servidor rodando na porta ${PORT}`);
     });
