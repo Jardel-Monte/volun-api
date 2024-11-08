e4027e50a74738b46493389e22a1178a9e9f2090
Jardel-Monte <JardelLima23@gmail.com>
2024-11-08 11:31:03 -0300

    Update Endereco.js

--- models/Endereco.js
+++ models/Endereco.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const mapearEventoParaAlgolia = require('./AlgoliaMapper');
+const index = require('../services/algoliaConfig');
 
 const enderecoSchema = new mongoose.Schema({
   logradouro: { type: String, required: true },
@@ -11,5 +13,24 @@ const enderecoSchema = new mongoose.Schema({
   org_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Organizacao', default: null },
   evento_id: { type: String, default: null },
 }, { timestamps: true }); 
+
+// Middleware para atualizar Algolia ao modificar evento_id
+enderecoSchema.post('save', async function (doc) {
+  if (doc.evento_id) {
+    try {
+      // Buscar o evento correspondente ao evento_id do endereço
+      const Evento = require('./Eventos');
+      const evento = await Evento.findById(doc.evento_id);
+
+      if (evento) {
+        // Mapear o evento atualizado para o Algolia e atualizar o Algolia
+        const eventoMapeado = await mapearEventoParaAlgolia(evento);
+        await index.saveObject(eventoMapeado);
+      }
+    } catch (error) {
+      console.error('Erro ao atualizar Algolia após modificação do endereço:', error);
+    }
+  }
+});
 
 module.exports = mongoose.model('Endereco', enderecoSchema);
