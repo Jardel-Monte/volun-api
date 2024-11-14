acfb97e54ca15f6c1f7c74aa07377e84361e717e
KevinJota <141651714+KevinJota@users.noreply.github.com>
2024-11-14 11:13:13 -0300

    Update participacaoRoutes.js

--- routes/participacaoRoutes.js
+++ routes/participacaoRoutes.js
@@ -5,7 +5,7 @@ const participacaoController = require('../controllers/participacaoController');
 router.post('/', participacaoController.createParticipacao);
 router.get('/', participacaoController.getParticipacoes);
 router.get('/usuario/:usuario_id', participacaoController.getParticipacoesByUsuario);
-router.get('/evento/:eventos_id', participacaoController.getParticipacoesByEvento);
+router.get('/evento/:evento_id', participacaoController.getParticipacoesByEvento);
 router.get('/status/:status', participacaoController.getParticipacoesByStatus);
 router.put('/:id', participacaoController.updateParticipacao);
 router.delete('/:id', participacaoController.deleteParticipacao);
