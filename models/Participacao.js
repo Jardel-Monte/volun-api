87db330185163b178c9734783ef3bbb890d2b006
KevinJota <141651714+KevinJota@users.noreply.github.com>
2024-11-14 11:12:56 -0300

    Update Participacao.js

--- models/Participacao.js
+++ models/Participacao.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const participacaoSchema = new mongoose.Schema({
     usuario_id: { type: String, required: true }, 
-    eventos_id:  { type: mongoose.Schema.Types.ObjectId, ref: 'Eventos', required: true }, // usa os eventos
+    evento_id:  { type: mongoose.Schema.Types.ObjectId, ref: 'Eventos', required: true }, // usa os eventos
     status: {type: String, default: 'confirmado'},
     data_inscricao: {type: Date, default: Date.now}
 }, { timestamps: true });
