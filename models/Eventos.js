cc737f88b2f5ba7df7290c5fb5cf1db26d4dfc0a
KevinJota <141651714+KevinJota@users.noreply.github.com>
2024-11-14 11:28:12 -0300

    Update Eventos.js

--- models/Eventos.js
+++ models/Eventos.js
@@ -5,9 +5,9 @@ const eventoSchema = new mongoose.Schema({
     descricao: { type: String, required: true },
     descricao_2: { type: String, required: false },
     tags: [{ type: String, default: null }],
-    data_inicio: { type: Date, required: true },
-    data_fim: { type: Date, required: true },
-    imagem: { type: String, required: true },
+    data_inicio: { type: Date, default: null },
+    data_fim: { type: Date, default: null },
+    imagem: { type: String, default: null },
     vaga_limite: { type: Number, required: true },
     ong_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Organizacao', default: null },
     endereco_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Endereco', default: null }  // Referência para 'Endereco'
