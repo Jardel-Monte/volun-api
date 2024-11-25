860c9534d5d1562af463eda051270151e6b7657e
KevinJota <141651714+KevinJota@users.noreply.github.com>
2024-11-25 11:57:32 -0300

    Add files via upload

--- routes/enderecoRoutes.js
+++ routes/enderecoRoutes.js
@@ -8,7 +8,6 @@ router.get('/:id', enderecoController.getEnderecoById);
 router.put('/:id', enderecoController.updateEndereco);
 router.delete('/:id', enderecoController.deleteEndereco);
 router.get('/usuario/:usuario_id', enderecoController.getEnderecoByUsuarioId);
-router.get('/evento/:evento_id', enderecoController.getEnderecoByEventoId);
 router.get('/org/:org_id', enderecoController.getEnderecoByOrgId);
 
 module.exports = router;
