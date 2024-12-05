001cebe8add8a443b343ae3dc03d93dfca72199f
KevinJota <141651714+KevinJota@users.noreply.github.com>
2024-12-05 00:12:15 -0300

    Update AcoesModeracao.js

--- models/AcoesModeracao.js
+++ models/AcoesModeracao.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const acoesModeracaoSchema = new mongoose.Schema({
     moderador_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true }, // Moderador que tomou a ação
     alvo_tipo: { type: String, required: true },  // 'usuario', 'evento', 'comentario', etc.
-    alvo_id: { type: mongoose.Schema.Types.ObjectId, required: true }, // ID do objeto alvo da ação (usuário, evento, etc.)
+    alvo_id: { type: String, required: true }, // ID do objeto alvo da ação (usuário, evento, etc.)
     acao: { type: String, required: true },  // 'excluir', 'restringir', 'advertir', etc.
     descricao: { type: String },  // Detalhes da ação (ex: motivo da exclusão, natureza da restrição, etc.)
     data: { type: Date, default: Date.now },  // Quando a ação foi realizada
